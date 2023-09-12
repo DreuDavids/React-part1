fa9e31baf22e9c4ca250f7a95f6e6b37bf3d8cd2
DreuDavids <dreu.davids1@gmail.com>
2023-09-12 16:29:55 +0200

    Part1.3 ~Changed Parts to objects

--- courseinfo/courseinfo/src/App.jsx
+++ courseinfo/courseinfo/src/App.jsx
@@ -1,12 +1,9 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
 
   const Header = () => {
     return <h1>{course}</h1>;
@@ -15,7 +12,7 @@ const App = () => {
   const Part = (props) => {
     return (
       <p>
-        {props.part} {props.exercise}
+        {props.part.name} {props.part.exercises}
       </p>
     );
   };
@@ -23,15 +20,20 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part part={part1} />
+        <Part part={part2} />
+        <Part part={part3} />
       </div>
     );
   };
 
   const Total = () => {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+    return (
+      <p>
+        Number of exercises{" "}
+        {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    );
   };
 
   return (
