ce7166cee938c64b1ad1583dba43a4ec549d280b
DreuDavids <dreu.davids1@gmail.com>
2023-09-12 17:05:21 +0200

    1.4 ~ course info step 4, Place objs into array

--- courseinfo/courseinfo/src/App.jsx
+++ courseinfo/courseinfo/src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = { name: "Fundamentals of React", exercises: 10 };
-  const part2 = { name: "Using props to pass data", exercises: 7 };
-  const part3 = { name: "State of a component", exercises: 14 };
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
-  const Header = () => {
-    return <h1>{course}</h1>;
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
   };
 
   const Part = (props) => {
@@ -17,30 +19,32 @@ const App = () => {
     );
   };
 
-  const Content = () => {
+  const Content = (props) => {
     return (
       <div>
-        <Part part={part1} />
-        <Part part={part2} />
-        <Part part={part3} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
       </div>
     );
   };
 
-  const Total = () => {
+  const Total = (props) => {
     return (
       <p>
         Number of exercises{" "}
-        {part1.exercises + part2.exercises + part3.exercises}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     );
   };
 
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
